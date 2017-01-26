bb480860019a8605b14aa1a2c28c9ebd21cd3937
Alberto Iannicca <Alberto Iannicca>
2017-01-26 18:04:28 +0100

    Aggiunta RegistratiActivity

--- app/src/main/java/android/vm/rentmycar/RegistratiActivity.java
+++ app/src/main/java/android/vm/rentmycar/RegistratiActivity.java
@@ -7,16 +7,15 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class RegistratiActivity extends Activity {
-    TextView nameTv;
+
     Intent intent;
-    String username;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrati);
-        nameTv=(TextView)findViewById(R.id.name_tv);
+
         intent = getIntent();
-        username = intent.getStringExtra("username");
-        nameTv.setText(username);
+
     }
 }
