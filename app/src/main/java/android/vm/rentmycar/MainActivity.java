80dec5250a7de30b30935748f49a3c633875c22a
Alberto Iannicca <Alberto Iannicca>
2017-01-27 09:53:26 +0100

    modificati main e registrati Activity

--- app/src/main/java/android/vm/rentmycar/MainActivity.java
+++ app/src/main/java/android/vm/rentmycar/MainActivity.java
@@ -21,27 +21,28 @@ import com.google.android.gms.common.api.GoogleApiClient;
 public class MainActivity extends Activity {
     //public static final String USERNAME_KEY="username"; usato per passare le chiavi senza scrivere il nome tra apici ma riportando la variabile statica.
     TextView usernTv,passTv ;
-    Button changeTextButton;
+    Button registratiButton;
     EditText changeTextEdit;
     MainActivity activity=this;
     String username;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        usernTv=(TextView) findViewById(R.id.usern_tv);//prendo il controllo della textview(presente in xml) dalla classe java
-        passTv=(TextView) findViewById(R.id.pass_tv);
-        changeTextButton = (Button) findViewById(R.id.change_text_button);
+        //usernTv=(TextView) findViewById(R.id.usern_tv);//prendo il controllo della textview(presente in xml) dalla classe java
+        //passTv=(TextView) findViewById(R.id.pass_tv);
+        registratiButton = (Button) findViewById(R.id.registrati_button);
 
-        changeTextEdit= (EditText)findViewById(R.id.change_text_edit);
+        //changeTextEdit= (EditText)findViewById(R.id.change_text_edit);
 
-        changeTextButton.setOnClickListener(new View.OnClickListener() {
+        registratiButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                // welcomeTv.setText(changeTextEdit.getText());
-                username=changeTextEdit.getText().toString();
+                //username=changeTextEdit.getText().toString();
                 Intent intent = new Intent(activity,RegistratiActivity.class);
-                intent.putExtra("username",username);
+                //intent.putExtra("username",username);
                 startActivity(intent);
             }
         });
