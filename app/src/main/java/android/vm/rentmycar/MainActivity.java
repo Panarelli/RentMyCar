80545ca0a49ce1ba47ef45dfdb5cb5bda47a4d7e
Alberto Iannicca <Alberto Iannicca>
2017-01-30 12:06:49 +0100

    Crash fixed

--- app/src/main/java/android/vm/rentmycar/MainActivity.java
+++ app/src/main/java/android/vm/rentmycar/MainActivity.java
@@ -20,26 +20,34 @@ import com.google.android.gms.common.api.GoogleApiClient;
 
 public class MainActivity extends Activity {
     //public static final String USERNAME_KEY="username"; usato per passare le chiavi senza scrivere il nome tra apici ma riportando la variabile statica.
-    TextView usernTv,passTv ;
-    Button registratiButton;
-    EditText changeTextEdit;
+
+    Button registratiButton, loginButton;
     MainActivity activity=this;
-    String username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         registratiButton = (Button) findViewById(R.id.btn_registrati);
+        loginButton = (Button) findViewById(R.id.btn_login);
 
-//COLLEGARE BOTTONE LOGIN A HOMEACTIVITY! <------------
         registratiButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
 
-                Intent intent = new Intent(activity,RegistratiActivity.class);
-                startActivity(intent);
+                Intent intent1 = new Intent(activity,RegistratiActivity.class);
+                startActivity(intent1);
+            }
+        });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                Intent intent2 = new Intent(activity,HomeActivity.class);
+                startActivity(intent2);
             }
         });
 
