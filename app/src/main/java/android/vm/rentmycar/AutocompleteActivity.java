c018d4da7562cdcafc9e33241381b8b18b9b0eee
fabio panarelli <fabio.panarelli0@gmail.com>
2017-02-01 09:17:45 +0100

    Revisione

---
+++ app/src/main/java/android/vm/rentmycar/AutocompleteActivity.java
@@ -0,0 +1,61 @@
+package android.vm.rentmycar;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.location.places.ui.PlaceAutocomplete;
+
+/**
+ * Created by panar on 30/01/2017.
+ */
+
+public class AutocompleteActivity extends FragmentActivity implements GoogleApiClient.OnConnectionFailedListener {
+    private GoogleApiClient mGoogleApiClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_autocomplete);
+
+        mGoogleApiClient = new GoogleApiClient
+                .Builder(this)
+                .addApi(Places.GEO_DATA_API)
+                .addApi(Places.PLACE_DETECTION_API)
+                .enableAutoManage(this, this)
+                .build();
+
+
+        int PLACE_AUTOCOMPLETE_REQUEST_CODE = 1;
+
+        try {
+            Intent intent =new PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_FULLSCREEN).build(this);
+            startActivityForResult(intent, PLACE_AUTOCOMPLETE_REQUEST_CODE);
+        } catch (GooglePlayServicesRepairableException e) {
+            // TODO: Handle the error.
+        } catch (GooglePlayServicesNotAvailableException e) {
+            // TODO: Handle the error.
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        setResult(Activity.RESULT_OK,data);
+        finish();
+    }
+}
--- app/src/main/java/android/vm/rentmycar/OfferActivity.java
+++ app/src/main/java/android/vm/rentmycar/OfferActivity.java
@@ -8,6 +8,9 @@ import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.EditText;
 
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocomplete;
+
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -19,7 +22,7 @@ public class OfferActivity extends Activity {
     Button inserisciAuto;
     Activity activity=this;
     ArrayList<String> cities;
-
+    EditText aggiungicitta;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_offer);
@@ -37,15 +40,30 @@ public class OfferActivity extends Activity {
             @Override
             public void onClick(View view) {
                 cities= new ArrayList<String>();
-                EditText chosecity = (EditText) findViewById(R.id.etaggiungicitta);
-                String s= ""+chosecity.getText();
-                cities.add(s);
-                for(int i=0;i<cities.size();i++){
-                    System.out.println(cities.get(i));
-                }
+                aggiungicitta = (EditText) findViewById(R.id.etaggiungicitta);
+
 
 
             }
         });
+        aggiungicitta= (EditText)findViewById(R.id.etaggiungicitta);
+        aggiungicitta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(activity,AutocompleteActivity.class);
+                startActivityForResult(intent,200);
+            }
+        });
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK && requestCode == 200){
+            Place place = PlaceAutocomplete.getPlace(this,data);
+            aggiungicitta.setText(place.getName());
+        }
     }
 }
