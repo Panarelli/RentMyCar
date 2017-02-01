c5b8005e73e5956758c1c59329846d8acba244db
fabio panarelli <fabio.panarelli0@gmail.com>
2017-02-01 13:11:38 +0100

    provadb

--- app/src/main/java/android/vm/rentmycar/MainActivity.java
+++ app/src/main/java/android/vm/rentmycar/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,6 +15,10 @@ import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.appindexing.Thing;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * Created by albof on 25/01/2017.
@@ -20,11 +26,14 @@ import com.google.android.gms.common.api.GoogleApiClient;
 
 public class MainActivity extends Activity {
     //public static final String USERNAME_KEY="username"; usato per passare le chiavi senza scrivere il nome tra apici ma riportando la variabile statica.
-    TextView usernTv,passTv ;
+    TextView usernTv, passTv;
     EditText changeTextEdit;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
 
     Button registratiButton, loginButton;
-    MainActivity activity=this;
+    MainActivity activity = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,27 +41,44 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         registratiButton = (Button) findViewById(R.id.btn_registrati);
         loginButton = (Button) findViewById(R.id.btn_login);
-
         registratiButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
 
-                Intent intent1 = new Intent(activity,RegistratiActivity.class);
+                Intent intent1 = new Intent(activity, RegistratiActivity.class);
                 startActivity(intent1);
             }
         });
 
 
-
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(activity,HomeActivity.class);
+                Intent intent = new Intent(activity, HomeActivity.class);
                 startActivity(intent);
             }
         });
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+
+
+        };
     }
 
 
-}
+
+    }
